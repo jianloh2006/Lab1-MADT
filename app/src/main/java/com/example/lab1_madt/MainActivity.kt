dabc573317f7f4977e5cb937d5d5967a736d0d49
Jay Loh <jaylohapple@gmail.com>
2026-09-24 00:07:25 +0300

    Updated Activity code file that handles button click and changes TextView

--- app/src/main/java/com/example/lab1_madt/MainActivity.kt
+++ app/src/main/java/com/example/lab1_madt/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lab1_madt
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener {
+            textView.setText(R.string.button_clicked)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
